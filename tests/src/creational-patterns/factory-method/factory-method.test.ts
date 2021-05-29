fc450698cf5e817def1bf02c72af877fc22393e1
Lucas Masayuki <lutamaribuchi@gmail.com>
2021-05-28 23:47:59 -0300

    Factory method tests

---
+++ tests/src/creational-patterns/factory-method/factory-method.test.ts
@@ -0,0 +1,19 @@
+import PokeballWithCharmander from '@/creational/factory-method/concreate/pokeball-with-charmander'
+import PokeballWithSquirtle from '@/creational/factory-method/concreate/pokeball-with-squirtle'
+import ThrowPokeball from '@/creational/factory-method/throw-pokeball'
+
+describe('Creational pattern - Fabric method', () => {
+    test('Should return eletric type, when inside pokeball has a Pikachu', async () => {
+        const pokeball = new PokeballWithCharmander()
+        const throwPokeball = new ThrowPokeball(pokeball)
+
+        expect(throwPokeball.getPokemonName()).toBe('charmander')
+    })
+
+    test('Should return eletric type, when inside pokeball has a Meowth', async () => {
+        const pokeball = new PokeballWithSquirtle()
+        const throwPokeball = new ThrowPokeball(pokeball)
+
+        expect(throwPokeball.getPokemonName()).toBe('squirtle')
+    })
+})
